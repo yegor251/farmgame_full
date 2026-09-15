c52b7ad729ca60d02556da321d0fdf8ec455b924
yegor251 <eagorov.yegor25@gmail.com>
2026-09-16 00:41:51 +0300

    bot

--- farmgame/bot.js
+++ farmgame/bot.js
@@ -15,7 +15,7 @@ bot.onText(/\/start/, (msg) => {
     const options = {
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'Open Web App', web_app: { url: 'https://farmgame-rclc.onrender.com' } }]
+                [{ text: 'Open Web App', web_app: { url: 'https://tonfarmg.site' } }]
             ]
         }
     };
